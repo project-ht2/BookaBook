f3eb05d3f7141161ed7e471813470734ed352861
Tanbui <hoangtan223@gmail.com>
2016-08-09 01:52:06 +0700

    Install Devise and Generate User

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def bootstrap_class_for flash_type
+		{ success: 'alert-success', error: 'alert-danger', notice: 'alert-warning'}[flash_type.to_sym]
+	end
+
+	def flash_messages(opts = {})
+		flash.map do |msg_type, message|
+			content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+				content_tag(:button, 'x'.html_safe, class: 'close', data: {dismiss: 'alert'}) + message
+			end
+		end.join.html_safe
+	end
 end
