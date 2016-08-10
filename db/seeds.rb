0f5bef0f865d746862a65f89725e677f7b41a90f
Hoang Huy Ngoc <hoanghuyngoc@gmail.com>
2016-08-10 08:56:10 +0000

    Add in Seed data for Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create Categories
+['Sách chính trị', 'Sách khoa học', 'Sách kinh doanh', 'Sách giáo dục', 'Sách thiếu nhi', 'Truyện', 'Thể loại khác'].each do |c|
+  Category.create(name: c)
+end
