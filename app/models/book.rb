4fb3d7e1477dd6e644fc4e91bb130b676f5219d6
Valentine <levantien83@gmail.com>
2016-08-13 05:11:03 +0000

    styling books/index view

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,13 +3,13 @@ class Book < ApplicationRecord
     belongs_to :category
   
     validates :title, presence: true
-    validates :isbn, uniqueness: true
+    validates :isbn, uniqueness: true, :allow_blank => true, :allow_nil => true
 
     def image_url_or_default
-        if image_url != nil 
-          return image_url 
+        if image_url.blank? 
+          "book_image/book_cover.jpg"
         else 
-          return "http://loremflickr.com/128/193/#{title}"
+          image_url
         end
     end
 end
