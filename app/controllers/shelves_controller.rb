8164dbe786c33b0d0ca38f5be6e81df5c1bc106a
Tanbui <hoangtan223@gmail.com>
2016-08-14 01:05:50 +0700

    Implement create shelf

---
+++ app/controllers/shelves_controller.rb
@@ -0,0 +1,21 @@
+class ShelvesController < ApplicationController
+  def new
+  	@shelf = Shelf.new
+  end
+
+  def create
+  	@shelf = Shelf.new shelf_params
+  	if @shelf.save
+  		flash[:success] = "Shelf created!"
+  		redirect_to user_index_path
+  	else
+  		flash[:error] = @shelf.errors.full_messages.to_sentence
+  		render 'new'
+  	end
+  end
+
+  private
+  def shelf_params
+  	params.require(:shelf).permit(:name, :description, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'users/index'
 
   resources :books
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   
   get 'home/index'
   root 'home#index'
+  resources :shelves
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
