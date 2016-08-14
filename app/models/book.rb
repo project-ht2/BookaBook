b67126f5144393cad1b22939bf739c9e6cf2cdd4
Valentine <levantien83@gmail.com>
2016-08-14 09:39:23 +0000

    styling books page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,16 @@
+  # create_table "books", force: :cascade do |t|
+  #   t.string   "title"
+  #   t.integer  "author_id"
+  #   t.integer  "category_id"
+  #   t.decimal  "isbn",        precision: 13
+  #   t.text     "description"
+  #   t.datetime "created_at",                 null: false
+  #   t.datetime "updated_at",                 null: false
+  #   t.string   "image_url"
+  #   t.index ["author_id"], name: "index_books_on_author_id", using: :btree
+  #   t.index ["category_id"], name: "index_books_on_category_id", using: :btree
+  # end
+
 class Book < ApplicationRecord
     belongs_to :author
     belongs_to :category
