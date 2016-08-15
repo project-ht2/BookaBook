4e599f2056c0500139a6554238296525b6e6f84f
Valentine <levantien83@gmail.com>
2016-08-15 22:44:25 +0000

    backgound method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def set_wooden_background
+    @background = "horizontal-wood-background" 
+  end
+  
+  def set_small_book_block
+    @col_wide = 'col-md-4'
+    @book_cover = 'book-cover'
+  end
+  
+  def set_big_book_block
+    @col_wide = 'col-md-12'
+    @book_cover = 'big-book-cover'
+  end
 end
