2e10e68938b364da4b7c33edd5403237d6fec282
Tanbui <hoangtan223@gmail.com>
2016-08-15 20:29:22 +0700

    Generate book review model

---
+++ app/models/book_review.rb
@@ -0,0 +1,13 @@
+=begin
+create_table "book_reviews", force: :cascade do |t|
+  t.integer  "user_id"
+  t.integer  "book_id"
+  t.integer  "rating"
+  t.text     "content"
+  t.datetime "created_at", null: false
+t.datetime "updated_at", null: false
+end
+=end
+
+class BookReview < ApplicationRecord
+end
