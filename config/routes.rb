0006bc0a76eb7b63cdf8100f4a222aff07ba6cd2
Tanbui <hoangtan223@gmail.com>
2016-08-17 01:22:11 +0700

    Add comment modal

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   resources :users do 
   	resources :shelves
   end
+  resources :book_reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
