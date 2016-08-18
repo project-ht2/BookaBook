430e595e7b23e7f5d8ae9b05153c21e36fe4464c
Valentine <levantien83@gmail.com>
2016-08-18 05:14:16 +0000

    book review working now

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -1,4 +1,7 @@
 class BookReviewsController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
+    @book_review = @book.book_reviews.create(params[:book_review].permit(:user_id, :rating, :content))
+    redirect_to @book
   end
 end
