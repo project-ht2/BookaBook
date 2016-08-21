76427e6c61b20f58d656ec0fc69298a8137caa4b
Hoang Huy Ngoc <hoanghuyngoc@gmail.com>
2016-08-21 03:26:04 +0000

    Pretty up Cart page

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -6,4 +6,12 @@ module TransactionsHelper
       return false
     end
   end
+  
+  def current_user_is_lender(transaction)
+    if current_user.id == transaction.book_item.shelf.user.id
+      return true
+    else
+      return false
+    end
+  end
 end
