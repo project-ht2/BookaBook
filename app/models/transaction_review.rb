cad28d79d81ba89ff9ba6477320c85a2283d476f
Tanbui <hoangtan223@gmail.com>
2016-08-23 23:45:48 +0700

    Generate Transaction review model

---
+++ app/models/transaction_review.rb
@@ -0,0 +1,2 @@
+class TransactionReview < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821165536) do
+ActiveRecord::Schema.define(version: 20160823164123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,15 @@ ActiveRecord::Schema.define(version: 20160821165536) do
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "transaction_reviews", force: :cascade do |t|
+    t.integer  "reviewer_id"
+    t.integer  "target_id"
+    t.integer  "rating"
+    t.text     "content"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
