3b37b7e2b5a8f4a6c19218383f1d405310f28725
Hoang Huy Ngoc <hoanghuyngoc@gmail.com>
2016-08-24 05:24:46 +0000

    Load message in conversion style

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,13 @@
 class CartsController < ApplicationController
+  helper_method :load_conversation
+  
   def show
     @borrower_transactions = Transaction.where(borrower_id: current_user.id)
     @lender_transactions = Transaction.joins(:book_item).merge(BookItem.joins(:shelf).merge(Shelf.where(user_id: current_user.id)))
     @current_user_messages = Message.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+  end
+  
+  def load_conversation(sender_id, receiver_id)
+      Message.where("(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)", sender_id, receiver_id, receiver_id, sender_id).order("created_at DESC")
   end
 end
