1d7772c016e29ad618d2edcf82d01c20ac8b9535
Tanbui <hoangtan223@gmail.com>
2016-08-28 12:43:21 +0700

    Add create review modal

--- app/controllers/transaction_reviews_controller.rb
+++ app/controllers/transaction_reviews_controller.rb
@@ -1,4 +1,15 @@
 class TransactionReviewsController < ApplicationController
   def create
+    build_review
+    @review.save
+  end
+
+  private
+  def review_params
+    params.require(:transaction_review).permit(:transaction_id, :reviewer_id, :target_id, :rating, :content)
+  end
+
+  def build_review
+    @review = TransactionReview.new review_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
   resources :users do 
   	resources :shelves
   end
+  post 'transaction_reviews' => 'transaction_reviews#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
