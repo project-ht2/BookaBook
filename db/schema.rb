89cdf400d8f1c8c2b8f00f2d4d1d5bbf7d8a6817
Tanbui <hoangtan223@gmail.com>
2016-09-02 11:12:11 +0700

    Generate User Group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902031806) do
+ActiveRecord::Schema.define(version: 20160902035721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,14 @@ ActiveRecord::Schema.define(version: 20160902031806) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_category_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "user_relationships", force: :cascade do |t|
