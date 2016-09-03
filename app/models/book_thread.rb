0cb89d4d966adf098febe0ae4737f565dbe23e22
Tanbui <hoangtan223@gmail.com>
2016-09-03 12:44:29 +0700

    Generate BookThread model and add seed data

---
+++ app/models/book_thread.rb
@@ -0,0 +1,4 @@
+class BookThread < ApplicationRecord
+  belongs_to :book
+  belongs_to :group, foreign_key: 'user_group_id', class_name: 'UserGroup'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903041402) do
+ActiveRecord::Schema.define(version: 20160903044453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 20160903041402) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_threads", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "book_id"
+    t.integer  "user_group_id"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "books", force: :cascade do |t|
